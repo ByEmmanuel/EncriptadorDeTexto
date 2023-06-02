b760c234e62560ff4446feb576a3ea35040d213c
Emma <polar1183@gmail.com>
2023-06-01 23:18:19 -0600

    Agregando mis archivos del proyecto

---
+++ Desafio-Encriptar.js
@@ -0,0 +1,30 @@
+
+var nombre = ("Emma");
+
+console.log(nombre);
+
+console.log(typeof nombre); //ver el tipo de variable en la consola
+
+var numero = 1000;
+
+function saltarlinea(salto){
+    document.write("<br>")
+    document.write("<br>")
+
+}
+
+function salto() {
+    saltarlinea
+}
+
+console.log(numero);
+
+console.log(typeof numero);
+
+function imprimir(frase) {
+    document.write(frase);
+}
+
+
+
+
